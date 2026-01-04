127cec350127dfc14ccad25dd84f9818697f04af
Priyansh shah <89834223+shah-priyansh@users.noreply.github.com>
2026-01-04 17:59:04 +0530

    Change to has routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
@@ -17,7 +17,7 @@ function App() {
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
